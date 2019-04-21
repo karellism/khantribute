cf6f633a1ae33d5c71a75c4e756e07162e3815b6
Karel Vanhelden <karelvanhelden@gmail.com>
2019-04-21 20:24:19 +0200

    Subject line (try to keep under 50 characters)
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,15 @@
-import { NgModule }      from '@angular/core';
-import { CommonModule }  from '@angular/common';
-import { FormsModule }   from '@angular/forms';
- 
-import { FilterTextComponent } from './filter-text/filter-text.component';
-import { FilterTextService }   from './filter-text/filter-text.service';
-import { InitCapsPipe }        from './init-caps.pipe';
- 
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   imports: [CommonModule, FormsModule],
   declarations: [
-    FilterTextComponent,
-    InitCapsPipe
   ],
-  providers: [FilterTextService],
+  providers: [],
   exports: [
     CommonModule,
     FormsModule,
-    FilterTextComponent,
-    InitCapsPipe
   ]
 })
 export class SharedModule { }
